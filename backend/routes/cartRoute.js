08fd65c8d8077bdf3d3277538fd4e8892e96c7bb
japhethh <boknoy32130@gmail.com>
2024-05-09 03:43:55 +0800

    Creating Shop Cart functionality

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,3 +1,12 @@
 import express from "express";
 import {addToCart,removeFromCart,getCart} from '../controllers/cartController.js'
-const ExpressRoute = express.Router();
+import authMiddleware from '../middleware/auth.js'
+
+const cartRouter = express.Router();
+cartRouter.post("/add",addToCart)
+cartRouter.post("/remove",removeFromCart)
+cartRouter.post("/list",getCart)
+
+export default cartRouter
+
+
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-  import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import 'dotenv/config'
 
 const app = express();
 const port = 4000;
@@ -20,6 +21,7 @@ connectDB();
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 app.get("/", (req, res) => {
   res.send("<h2 className='text-orange-500'>API Workingf</h2>");
