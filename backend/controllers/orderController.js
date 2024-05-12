08d1c6c38aa09ae2fcdae12acd48f784331947bb
japhethh <boknoy32130@gmail.com>
2024-05-13 02:33:16 +0800

    Finish setUp orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,6 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order for frontend
 const placeOrder = async (req,res) => {
+  const  frontend_url = "http://localhost:5173" 
+
   try {
     const newOrder = new orderModel({
       userId:req.body.userId,
@@ -22,10 +24,34 @@ const placeOrder = async (req,res) => {
         product_data:{
           name:item.name
         },
-        unit
-      }
+        unit_amount:item.price *100*80
+      },
+     quantity:item.quantity
     }))
+
+    line_items.push({
+      price_data:{
+        currency:"inr",
+        product_data:{
+          name:"Delivery"
+        },
+        unit_amount:2*100*80
+      },
+      quantity:1
+    })
+
+
+    const session = await stripe.checkout.sessions.create({
+      line_items:line_items,
+      mode:'payment',
+      success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancell_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    })
+
+    res.json({success:true,sessino_url:session.url})
   } catch (error) {
+    console.log(error)
+    res.json({success:false,message:"Error"});
     
   }
 }
